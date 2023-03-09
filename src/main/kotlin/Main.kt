8c988a9ebb368ef8e285b951508ad710e55c1ed0
AmakoveBridgit <amakovebridgit@gmail.com>
2023-03-09 08:55:38 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,65 @@
+fun main() {
+println()
+   cars()
+    add()
+    subtract(
+
+
+
+
+
+
+}
+fun personalities(nameA:String,nameB:String):String{
+    var names=nameA
+    return names
+
+
+}
+fun integers(max:Int,min:Int,ave:Int):Double{
+    val integ= arrayOf(20,30,40,50,60)
+    var max=integ.max()
+    var min=integ.min()
+    var aver=integ.average()
+
+}
+fun cars(){
+    var name="Toyota"
+    println(name.split(""))
+
+}
+fun counties(array: Array<String>):String{
+    var names= arrayOf("Mombasa","Kisumu","Nairobi","Kakamega")
+    return names
+}
+class Numbers(var num1:Int,var num2:Int,var num3:Int,var num4){
+    var numbers=Numbers(20,30,40,25)
+
+}
+fun add(){
+    var num1=20
+    var num2=30
+    var sum=num1 +num2
+ println(sum)
+}
+fun subtract(){
+    var num1=20
+    var num2=30
+    var difference=num1 -num2
+    println(difference)
+
+}
+fun division(){
+    var num2=30
+    var num3=25
+    var divison=num2 /num3
+    println(divison)
+}
+
+
+
+
+
+
+
+
